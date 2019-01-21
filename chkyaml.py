691008ac08cf876ef027935f00b4e82e8d942a84
ZomboKid <38490271+ZomboKid@users.noreply.github.com>
2019-01-21 12:59:29 +0300

    Update chkyaml.py

--- chkyaml.py
+++ chkyaml.py
@@ -1,15 +1,20 @@
 #! /usr/bin/python
 
-import sys, yaml
+import sys
+import yaml
 
-file_path=sys.argv[1] #set filename in command line argument
-#---------------------------------------------------------------------
+file_path = sys.argv[1]  # set filename in command line argument
+# ---------------------------------------------------------------------
+
+
 def f_check_yaml_file():
     try:
-        yaml_data=yaml.safe_load(open(file_path,'r'))
+        yaml_data = yaml.safe_load(open(file_path, 'r'))
     except Exception as e:
-        print >> sys.stderr, "Failed parse yaml file: %s" %e
+        print >> sys.stderr, "Failed parse yaml file: %s" % e
         sys.exit(1)
-#---------------------------------------------------------------------
-if __name__=="__main__":
+# ---------------------------------------------------------------------
+
+
+if __name__ == "__main__":
     f_check_yaml_file()
